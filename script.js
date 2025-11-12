493288ad65f832261561cd06f48a4029479d7771
leirsam <leirsam@gmail.com>
2025-11-12 17:23:07 +0100

    added the resulys of the game in DOM and changed the computer's choice each time the palyer plays a game, also added h2 tag via js to announce the round

--- script.js
+++ script.js
@@ -12,65 +12,78 @@ function getComputerChoice() {
     }
 }
 
+let pChoice = "";
+let result = "";
+
 function processGame(CC, playerChoice) {
-      if (CC == "scissors") {
-        switch (playerChoice) {
-        case "scissors":
-        console.log("it's a Tie!")
-        break;
+        pChoice = playerChoice;
+        if (CC == "scissors") {
+            switch (playerChoice) {
+            case "scissors":
+            // console.log("it's a Tie!");
+            result = "it's a Tie!"
+            break;
 
-        case "rock":
-        console.log("Congrats! You win");
-        // humanScore++;
-        break;
+            case "rock":
+            result = "Congrats! You win";
+            // console.log("Congrats! You win");
+            // humanScore++;
+            break;
 
-        case "paper":
-        console.log("the Computer wins! Sorry")
-        // computerScore++;
-        break;
+            case "paper":
+            result = "The Computer wins! Sorry";
+            // console.log("the Computer wins! Sorry");
+            // computerScore++;
+            break;
+
+            default:
+            console.log("you need to correct you play please!")
+            }
+    }       else if (CC == "rock") {
+            switch (playerChoice) {
+            case "rock":
+            result = "it's a Tie!"
+            // console.log("it's a Tie!")
+            break;
+
+            case "paper":
+            result = "Congrats! You win";
+            // console.log("Congrats! You win");
+            // humanScore++;
+            break;
+
+            case "scissors":
+            result = "the Computer wins! Sorry";
+            console.log("The Computer wins! Sorry")
+            // computerScore++;
+            break;
+
+            default:
+            console.log("you need to correct you play please!")
+            }
+
+    }       else {
+            switch (playerChoice) {
+            case "paper":
+            result = "it's a Tie!"
+            // console.log("it's a Tie!")
+            break;
+
+            case "scissors":
+            result = "Congrats! You win";
+            // console.log("Congrats! You win");
+            // humanScore++;
+            break;
+
+            case "rock":
+            result = "The Computer wins! Sorry";            // console.log("the Computer wins! Sorry")
+            // computerScore++;
+            break;
 
         default:
         console.log("you need to correct you play please!")
-        }
-    } else if (CC == "rock") {
-        switch (playerChoice) {
-        case "rock":
-        console.log("it's a Tie!")
-        break;
-
-        case "paper":
-        console.log("Congrats! You win");
-        // humanScore++;
-        break;
-
-        case "scissors":
-        console.log("the Computer wins! Sorry")
-        // computerScore++;
-        break;
-
-        default:
-        console.log("you need to correct you play please!")
-        }
-
-    } else {
-        switch (playerChoice) {
-        case "paper":
-        console.log("it's a Tie!")
-        break;
-
-        case "scissors":
-        console.log("Congrats! You win");
-        // humanScore++;
-        break;
-
-        case "rock":
-        console.log("the Computer wins! Sorry")
-        // computerScore++;
-        break;
-
-        default:
-        console.log("you need to correct you play please!")
-        }
+        
+    }
     
     }
       
@@ -83,25 +96,55 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 
-let CC = getComputerChoice();
+let CC = ""
+let numberGames = 0;
+
+function displayResult() {
+        const div = document.createElement("diu");
+        const h2 = document.createElement("h2");
+        const p1 = document.createElement("p");
+        const p2 = document.createElement("p");
+
+        h2.textContent = `Game N° ${numberGames}`
+        p1.textContent = `You Choice is: ${pChoice}, The Computer's choice is: ${CC}.`;
+        p2.textContent = result;
+
+        p1.style.backgroundColor = "lightblue"
+
+        document.body.appendChild(div);
+        div.append (h2);
+        div.appendChild (p1);
+        div.appendChild (p2);
+}
+
+
 
 rock.addEventListener("click", () => {
-    processGame(CC, "rock")
-    console.log("You Choice is: rock, The Computer's choice is:" + CC);
+    CC = getComputerChoice();
+    processGame(CC, "rock");
+    numberGames++;
+    displayResult();
+    // console.log("You Choice is: rock, The Computer's choice is:" + CC);
 }
 );
+
 paper.addEventListener("click", () => {
-    processGame(CC, "paper")
-    console.log("You Choice is: paper, The Computer's choice is:" + CC);
+    CC = getComputerChoice();
+    processGame(CC, "paper");
+    numberGames++;
+    displayResult();
+    // console.log("You Choice is: paper, The Computer's choice is:" + CC);
 }
 );
+
 scissors.addEventListener("click", () => {
-    processGame(CC, "scissors")
-    console.log("You Choice is: scissors, The Computer's choice is:" + CC);
+    CC = getComputerChoice();
+    processGame(CC, "scissors");
+    numberGames++;
+    displayResult();
+    // console.log("You Choice is: scissors, The Computer's choice is:" + CC);
 }
 );
-
-
 
 
 
@@ -115,5 +158,3 @@ scissors.addEventListener("click", () => {
 //   console.log("You Choice is: " + playerChoice + ", The Computer's choice is:" + CC);
 //   processGame(CC, playerChoice);
 // }
-
-
